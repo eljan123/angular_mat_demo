e340275c672011e06f941519196ff77272f5af59
SJH-706 <sjh-706@SJH-706-22.local>
2025-02-26 10:40:36 +0800

    pushing register component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { RegisterComponent } from './register/register.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, RegisterComponent, ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/register/register.component.ts
@@ -0,0 +1,69 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormGroup, FormControl, Validators, FormsModule } from '@angular/forms';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+
+@Component({
+  selector: 'app-register',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule, FormsModule, MatButtonModule, MatCheckboxModule, MatFormFieldModule, MatInputModule, MatSliderModule, MatRadioModule, MatDatepickerModule, MatNativeDateModule],
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.css'
+})
+export class RegisterComponent {
+  userName:string = '';
+  email:string = '';
+  password:string = '';
+  gender:string = '';
+  birthDate!: Date;
+  address:string = '';
+  angularSkillLevel:number = 5;
+  submitted = false;
+  minSkillLevel = 1;
+  maxSkillLevel = 10;
+
+  formData: FormGroup = new FormGroup({
+    userName:new FormControl(''),
+    email:new FormControl('', [Validators.required, Validators.email]),
+    password:new FormControl('', [Validators.required, Validators.minLength(8)]),
+    gender:new FormControl('', [Validators.required]),
+    birthDate:new FormControl(null, [Validators.required]),
+    address:new FormControl(''),
+    angularSkillLevel:new FormControl(5)
+  })
+
+  onClickSubmit(data: {
+    userName: string;
+    email: string;
+    password: string;
+    gender: string;
+    address: string;
+    birthDate: Date;
+    angularSkillLevel: number;
+  })
+  {
+    this.submitted = true;
+    this.userName = data.userName;
+    this.email = data.email;
+    this.password = data.password;
+    this.address = data.address;
+    this.angularSkillLevel = data.angularSkillLevel;
+    this.birthDate = data.birthDate;
+
+    if (this.formData.valid){
+      console.log('Form submitted successfully!', this.formData.value);
+    }
+    else{
+      console.log('Form is not valid!')
+    }
+  }
+}
